1214ddb1542a2f309ccb977208e17e86be82217e
James Henstridge <james@jamesh.id.au>
2015-02-05 10:43:29 +0800

    Add documentation.

--- accounts.go
+++ accounts.go
@@ -18,6 +18,12 @@ type accountDetails struct {
         LoginFailedAction PostLoginAction `json:"login_failed_action"`
 }
 
+// RegisterAccountLoginResult configures a result such that the dash
+// will attempt to log in to the account identified by (serviceName,
+// serviceType, providerName).
+//
+// On success, the dash will perform the action specified by
+// passedAction.  On failure, it will use failedAction.
 func RegisterAccountLoginResult(result *CategorisedResult, query *CannedQuery, serviceName, serviceType, providerName string, passedAction, failedAction PostLoginAction) error {
 	if result.URI() == "" {
 		if err := result.SetURI(query.ToURI()); err != nil {
