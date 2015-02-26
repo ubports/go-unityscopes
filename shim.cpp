f2d38e44c58065e749c3a26301d270eff5e26990
James Henstridge <james@jamesh.id.au>
2015-02-26 18:10:09 +0800

    Split out ActivationResponse shim methods.

--- shim.cpp
+++ shim.cpp
@@ -309,22 +309,3 @@ void *action_metadata_get_scope_data(_ActionMetadata *metadata, int *data_length
     const std::string data = reinterpret_cast<ActionMetadata*>(metadata)->scope_data().serialize_json();
     return as_bytes(data, data_length);
 }
-
-void activation_response_init_status(_ActivationResponse *response, int status) {
-    *reinterpret_cast<ActivationResponse*>(response) =
-        ActivationResponse(static_cast<ActivationResponse::Status>(status));
-}
-
-void activation_response_init_query(_ActivationResponse *response, _CannedQuery *query) {
-    *reinterpret_cast<ActivationResponse*>(response) =
-        ActivationResponse(*reinterpret_cast<CannedQuery*>(query));
-}
-
-void activation_response_set_scope_data(_ActivationResponse *response, char *json_data, int json_data_length, char **error) {
-    try {
-        Variant v = Variant::deserialize_json(std::string(json_data, json_data_length));
-        reinterpret_cast<ActivationResponse*>(response)->set_scope_data(v);
-    } catch (const std::exception &e) {
-        *error = strdup(e.what());
-    }
-}
