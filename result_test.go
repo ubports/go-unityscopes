ae28888f280afe12eee072ddf4cf83aa87be8ad7
James Henstridge <james@jamesh.id.au>
2015-03-02 14:16:31 +0800

    Add tests for more result methods.

--- result_test.go
+++ result_test.go
@@ -14,3 +14,61 @@ func (s *S) TestResultSetURI(c *C) {
 	c.Check(r.Get("uri", &uri), IsNil)
 	c.Check(uri, Equals, "http://example.com")
 }
+
+func (s *S) TestResultSetTitle(c *C) {
+	r := scopes.NewTestingResult()
+	c.Check(r.SetTitle("The title"), IsNil)
+	c.Check(r.Title(), Equals, "The title")
+
+	var title string
+	c.Check(r.Get("title", &title), IsNil)
+	c.Check(title, Equals, "The title")
+}
+
+func (s *S) TestResultSetArt(c *C) {
+	r := scopes.NewTestingResult()
+	c.Check(r.SetArt("http://example.com/foo.png"), IsNil)
+	c.Check(r.Art(), Equals, "http://example.com/foo.png")
+
+	var uri string
+	c.Check(r.Get("art", &uri), IsNil)
+	c.Check(uri, Equals, "http://example.com/foo.png")
+}
+
+func (s *S) TestResultSetDndURI(c *C) {
+	r := scopes.NewTestingResult()
+	c.Check(r.SetDndURI("http://example.com"), IsNil)
+	c.Check(r.DndURI(), Equals, "http://example.com")
+
+	var uri string
+	c.Check(r.Get("dnd_uri", &uri), IsNil)
+	c.Check(uri, Equals, "http://example.com")
+}
+
+func (s *S) TestResultSetComplexValue(c *C) {
+	type Attr struct {
+		Value string `json:"value"`
+	}
+
+	r := scopes.NewTestingResult()
+	c.Check(r.Set("attributes", []Attr{
+		Attr{"one"},
+		Attr{"two"},
+	}), IsNil)
+
+	// Check that the value has been encoded as expeected:
+	var v interface{}
+	c.Check(r.Get("attributes", &v), IsNil)
+	c.Check(v, DeepEquals, []interface{}{
+		map[string]interface{}{"value": "one"},
+		map[string]interface{}{"value": "two"},
+	})
+
+	// The value can also be decoded into the complex structure too
+	var v2 []Attr
+	c.Check(r.Get("attributes", &v2), IsNil)
+	c.Check(v2, DeepEquals, []Attr{
+		Attr{"one"},
+		Attr{"two"},
+	})
+}
