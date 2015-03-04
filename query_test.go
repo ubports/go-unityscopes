d3a320d69d81ec322b63b45e3f62c63f7a386ead
Xavi Garcia <xavi.garcia.mena@canonical.com>
2015-03-04 14:59:15 +0100

    Changed Assert for Check in tests

--- query_test.go
+++ query_test.go
@@ -9,19 +9,19 @@ func (s *S) TestQuery(c *C) {
 	query := scopes.NewCannedQuery("scope", "query_string", "department_string")
 
 	// basic check
-	c.Assert(query.ScopeID(), Equals, "scope")
-	c.Assert(query.DepartmentID(), Equals, "department_string")
-	c.Assert(query.QueryString(), Equals, "query_string")
+	c.Check(query.ScopeID(), Equals, "scope")
+	c.Check(query.DepartmentID(), Equals, "department_string")
+	c.Check(query.QueryString(), Equals, "query_string")
 
 	// verify uri
-	c.Assert(query.ToURI(), Equals, "scope://scope?q=query%5Fstring&dep=department%5Fstring")
+	c.Check(query.ToURI(), Equals, "scope://scope?q=query%5Fstring&dep=department%5Fstring")
 
 	// check setters
 	query.SetDepartmentID("department_id")
-	c.Assert(query.DepartmentID(), Equals, "department_id")
+	c.Check(query.DepartmentID(), Equals, "department_id")
 
 	query.SetQueryString("new_query_value")
-	c.Assert(query.QueryString(), Equals, "new_query_value")
+	c.Check(query.QueryString(), Equals, "new_query_value")
 
 	// TODO FilterState setter
 }
