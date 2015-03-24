2e12252537d58138e5d1f48ea754bfe378fa6b18
Xavi Garcia <xavi.garcia.mena@canonical.com>
2015-03-24 12:05:15 +0100

    Modified static_assert as it was not correct

--- version.h
+++ version.h
@@ -4,8 +4,8 @@
 #include <unity/scopes/Version.h>
 
 // check that we have a compatible version of lib-unityscopes installed
-static_assert(UNITY_SCOPES_VERSION_MAJOR >= 0 &&
-              UNITY_SCOPES_VERSION_MINOR >= 6 &&
-              UNITY_SCOPES_VERSION_MICRO >= 9, "Version of Unity scopes API mismatch. Minimum required version is 0.6.9.");
+static_assert(UNITY_SCOPES_VERSION_MAJOR > 0 ||
+             (UNITY_SCOPES_VERSION_MAJOR == 0 && (UNITY_SCOPES_VERSION_MINOR > 6 || (UNITY_SCOPES_VERSION_MINOR == 6 && UNITY_SCOPES_VERSION_MICRO >= 9))),
+              "Version of Unity scopes API mismatch. Minimum required version is 0.6.9.");
 
 #endif
